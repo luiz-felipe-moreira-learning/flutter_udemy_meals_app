9ac10b61045ef42cbd9ec3f2e57c9ce8d8f7a3c0
Luiz Felipe Sampaio Castelo Branco Moreira <luiz-felipe.moreira@serpro.gov.br>
2020-01-24 10:42:53 -0300

    Navigation to new page + passinga data

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: Center(
+        child: Text(
+          'The Recipes For The Category!',
+        ),
+      ),
+    );
+  }
+}
